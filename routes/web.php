59af6b6c9fd057c9e17acde6522e1926c79ce1c0
d43 <d43m13n@outlook.com>
2025-12-30 22:26:40 +0100

    first route/jobs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/jobs', function() {
+    return '<h1>Available Jobs</h1>';
+})->name('jobs');
+
