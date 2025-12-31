8cdff2a0893b6f404a614de5fddee3041df9cace
d43 <d43m13n@outlook.com>
2025-12-31 03:44:15 +0100

    added homeController and refactored routes in web.php

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index () 
+    {
+        $title = 'Available Jobs';
+        $jobs = [
+            'Web Developer',
+            'Linux System Admin',
+            'Php Developer',
+            'Ruby Developer',
+        ];
+
+    return view('jobs.index',  compact('title', 'jobs'));    
+    }
+
+
+    public function create () 
+    {
+        return view('jobs.create');
+    }
+}
+    
