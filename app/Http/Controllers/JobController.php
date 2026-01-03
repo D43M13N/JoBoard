fe5f9386025839e168cf411df985eda318275a5f
d43 <d43m13n@outlook.com>
2026-01-03 01:21:48 +0100

    refactor jobcontroller crud methods scaffolding

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,42 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class JobController extends Controller
 {
-    public function index () 
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
     {
-        $title = 'Available Jobs';
         $jobs = [
             'Web Developer',
-            'Linux System Admin',
-            'Php Developer',
+            'PHP Developer',
             'Ruby Developer',
+            'Linux Administrator',
         ];
-
-    return view('jobs.index',  compact('title', 'jobs'));    
+        return view('jobs.index', compact($jobs));
     }
 
-
-    public function create () 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
     {
         return view('jobs.create');
     }
 
-        public function show (string $id) 
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
     {
-        return "This is job $id";
+        return "store";
     }
 
-    public function store (Request $request)
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): string
     {
-        $title = $request->input('title');
-        $description = $request->input('description');
+        return "show";
 
-        return "Title: $title, Description: $description";
-
-        
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): string
+    {
+        return "Edit";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): string
+    {
+        return "Update";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): string
+    {
+        return "Destroy";
+    }
 }
-    
