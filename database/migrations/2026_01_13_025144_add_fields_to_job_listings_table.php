3a30269c925dbd23233bd80186b02d4e1454e963
d43 <d43m13n@outlook.com>
2026-01-13 04:24:50 +0100

    added fields to job_listings table

---
+++ database/migrations/2026_01_13_025144_add_fields_to_job_listings_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        DB::table('job_listings')->truncate();
+
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->integer('salary');
+            $table->string('tags');
+            $table->enum('job_type', ['Full-time', 'Part-time', 'Contract', 'Temporary', 'Internship', 'Volunteer', 'On-Call'])->default('Full-time');
+            $table->boolean('remote')->default(false);
+            $table->string('requirements')->nullable();
+            $table->string('benefits')->nullable();
+            $table->string('adress')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode')->nullable();
+            $table->string('contact_phone')->nullable();
+            $table->string('company_name');
+            $table->string('company_description')->nullable();
+            $table->string('company_logo')->nullable();
+            $table->string('company_website')->nullable();
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->dropColumn(['salary', 'tags', 'job_type', 'remote', 'requirements', 'benefits', 'address', 
+                                'city', 'state', 'zipcode', 'contact_email', 'contact_phone', 'company_name',
+                                'company_description', 'company_logo', 'company_website'
+    
+    
+                            ]);
+        });
+    }
+};
